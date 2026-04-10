7228c7e41869e959497a28b0d5b2568cdf4d3ab9
geekbraines <morozovol@yandex.ru>
2026-04-10 10:36:01 +0300

    last

--- js/script.js
+++ js/script.js
@@ -168,3 +168,17 @@ $(document).ready(function () {
         adjustTabPadding(); // Обновляем отступы после полной загрузки
     });
 });
+
+$(document).ready(function() {
+    $('a[href^="#"]').on('click', function(e) {
+        e.preventDefault();
+        
+        var target = $(this.hash);
+        
+        if (target.length) {
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 800);
+        }
+    });
+});
